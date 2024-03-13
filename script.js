4c7314508c7b8e874debcb43bbc53f7c673a4797
swr9 <133266144+swr9@users.noreply.github.com>
2024-03-13 18:26:21 -0400

    added function playerRound that compares getComputerChoice to playerSelection

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-function getComputerChoice()
+let gesture = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  let randomIndex = Math.floor(Math.random() * 3);
+  return gesture[randomIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    return "Player wins!";
+  } else if (
+    (playerSelection === "scissors" && computerSelection === "rock") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "rock" && computerSelection === "paper")
+  ) {
+    return "Player loses...";
+  } else {
+    return "Its a tie!";
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
